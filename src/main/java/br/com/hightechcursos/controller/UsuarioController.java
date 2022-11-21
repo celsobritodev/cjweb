91dff32e9beaad502e23487743358cd3acee700f
celsobritodev <celsinhobrito@gmail.com>
2022-11-21 08:39:18 -0300

    servlet em acao

--- src/main/java/br/com/hightechcursos/controller/UsuarioController.java
+++ src/main/java/br/com/hightechcursos/controller/UsuarioController.java
@@ -1,14 +1,16 @@
 package br.com.hightechcursos.controller;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-/**
- * Servlet implementation class UsuarioController
- */
+
+
+//@WebServlet("/usucontroller.do")
 public class UsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -24,15 +26,24 @@ public class UsuarioController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Chamando o metodo doget");
+		
+		
+		String nome = request.getParameter("nome");
+		System.out.println("Nome: "+nome);
+		
+		String empresa = request.getParameter("empresa");
+	    System.out.println("Empresa: "+empresa);
+	    
+	    PrintWriter saida = response.getWriter();
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Chamando o metodo dopost");
 		doGet(request, response);
 	}
 
